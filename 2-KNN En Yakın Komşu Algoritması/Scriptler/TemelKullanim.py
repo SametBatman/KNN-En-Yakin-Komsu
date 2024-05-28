e3bf40bdca3519b8247fc28f2c787648e1072c47
SametBatman <160470839+SametBatman@users.noreply.github.com>
2024-05-28 11:38:49 +0300

    Update TemelKullanim.py

--- 2-KNN En Yakın Komşu Algoritması/Scriptler/TemelKullanim.py
+++ 2-KNN En Yakın Komşu Algoritması/Scriptler/TemelKullanim.py
@@ -1,25 +1,29 @@
 import pandas as pd  # Verileri işleme ve analiz etme kütüphanesi
-from sklearn.model_selection import train_test_split  # Verileri eğitim ve test kümelerine ayırma fonksiyonu
-from sklearn.neighbors import KNeighborsClassifier  # K-En Yakın Komşu sınıflandırma algoritması
+from sklearn.model_selection import train_test_split  # Verileri eğitim ve test kümelerine ayırma fonksiyonu içerir.
+from sklearn.neighbors import KNeighborsClassifier  # K-En Yakın Komşu sınıflandırma algoritmasını içerir.
 
-# Verileri yükle
+# Verileri yüklemek
 data = pd.read_csv("data.csv")  # CSV dosyasından veri okunur
 
-# Özellikleri ve hedef değişkeni ayır
+# data değişkeninden "öznitelik1", "öznitelik2", ... gibi özelliklerin bulunduğu X adlı bir veri çerçevesi oluşturulur.
+# data değişkeninden "hedef_değişken" adlı hedef değişken ayrılarak y adlı bir değişkene atanır.
 X = data[["öznitelik1", "öznitelik2", ...]]  # Özellikler seçilir
 y = data["hedef_değişken"]  # Hedef değişken seçilir
 
-# Verileri eğitim ve test kümelerine ayır
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)  # Veriler %80 eğitim, %20 test kümesi olarak ayrılır
+# Eğitim kümesi X_train ve y_train değişkenlerine, test kümesi ise X_test ve y_test değişkenlerine atanır.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)  # Veriler train_test_split %80 eğitim, %20 test kümesi olarak ayrılır
 
-# KNN modelini oluştur ve eğit
+# KNeighborsClassifier sınıfı kullanılarak bir KNN modeli oluşturulur.
+# n_neighbors parametresi 5 olarak ayarlanır, bu da modelin sınıflandırma işleminde en yakın 5 komşunun oy kullanacağı anlamına gelir.
+
 knn = KNeighborsClassifier(n_neighbors=5)  # KNN modeli oluşturulur, k değeri 5 olarak belirlenir
-knn.fit(X_train, y_train)  # Model eğitim verisi üzerinde eğitilir
+knn.fit(X_train, y_train)  # Oluşturulan model X_train ve y_train eğitim verisi üzerinde eğitilir.
 
-# Yeni bir veri noktasını sınıflandır
+# yeni_değer1 ve yeni_değer2 gibi yeni veri noktasının değerleri belirlenir.
 yeni_değer1 = 1.2  # Yeni veri noktasının ilk değeri
 yeni_değer2 = 1.5  # Yeni veri noktasının ikinci değeri
-yeni_veri = [yeni_değer1, yeni_değer2, ...]  # Yeni veri noktası bir listeye dönüştürülür
+yeni_veri = [yeni_değer1, yeni_değer2, ...]  # Yeni veri noktası "yeni_veri" adlı bir listeye dönüştürülür.
+#Eğitimli KNN modeli yeni_veri listesini kullanarak yeni veri noktasının sınıfını tahmin eder.
 
 # Tahmini yazdır
 tahmin = knn.predict(yeni_veri)  # Yeni veri noktası için sınıf tahmini yapılır
